3e8402c57cca626b7dbd6119ac6cbab9031d4e92
LucasCavalheri <cav.catecumenal@gmail.com>
2023-11-02 12:01:01 -0300

    feat: add new note

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,27 +1,37 @@
 <template>
 	<div class="notes">
-		<div class="card has-background-success-dark p-4 mb-5">
+		<form @submit.prevent>
+			<div class="card has-background-success-dark p-4 mb-5">
+				<div class="field">
+					<div class="control">
+						<textarea
+							class="textarea"
+							placeholder="Add a new note!"
+							ref="newNoteRef"
+							v-model="newNote"
+						/>
+					</div>
+				</div>
 
-			<div class="field">
-				<div class="control">
-					<textarea class="textarea" placeholder="Add a new note!" />
+				<div class="field is-grouped is-grouped-right">
+					<div class="control">
+						<button
+							type="submit"
+							class="button is-link has-background-success"
+							:disabled="!newNote || newNote.split(' ').join('').length === 0"
+							@click="addNote"
+						>
+							Add New Note
+						</button>
+					</div>
 				</div>
 			</div>
+		</form>
 
-			<div class="field is-grouped is-grouped-right">
-				<div class="control">
-					<button class="button is-link has-background-success">Add New Note</button>
-				</div>
-			</div>
-		</div>
-
-		<div class="card mb-5" v-for="i in 3">
+		<div class="card mb-5" v-for="note in notes" :key="note.id">
 			<div class="card-content">
 				<div class="content">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-					quidem fugit magni repellat, rerum id porro natus quibusdam eius
-					velit? Aliquam animi quasi fugit facilis distinctio similique sequi
-					iste iusto!
+					{{ note.content }}
 				</div>
 			</div>
 			<footer class="card-footer">
@@ -32,10 +42,35 @@
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const newNote = ref('');
+const newNoteRef = ref(null);
+
+const notes = ref([
+	{
+		id: 'id1',
+		content: 'This is a long content of note 1'
+	},
+	{
+		id: 'id2',
+		content: 'Content of note 2'
+	}
+]);
+
+const addNote = () => {
+	notes.value.unshift({
+		id: Math.random().toString(),
+		content: newNote.value
+	});
+	newNote.value = '';
+	newNoteRef.value.focus();
+};
+</script>
 
 <style lang="scss" scoped>
 textarea {
-  resize: none;
+	resize: none;
 }
 </style>
