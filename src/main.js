660b853df9926639ca77a07e9b5efee371244f77
LucasCavalheri <cav.catecumenal@gmail.com>
2023-11-06 18:31:52 -0300

    feat: redirect user on auth change

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp, markRaw } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const pinia = createPinia();
 
-app.use(createPinia())
-app.use(router)
+pinia.use(({ store }) => {
+	store.router = markRaw(router);
+});
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+app.mount('#app');
