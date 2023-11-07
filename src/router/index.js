74201bced94e428c1302ea5ff6678621412a71e9
LucasCavalheri <cav.catecumenal@gmail.com>
2023-11-06 21:25:41 -0300

    feat: add reset-password flow

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import ViewAuth from '@/views/ViewAuth.vue';
 import ViewEditNote from '@/views/ViewEditNote.vue';
 import ViewNotes from '@/views/ViewNotes.vue';
 import ViewStats from '@/views/ViewStats.vue';
+import ViewForgotPassword from '@/views/ViewForgotPassword.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
@@ -27,12 +28,23 @@ const router = createRouter({
 			path: '/auth',
 			name: 'Auth',
 			component: ViewAuth
+		},
+		{
+			path: '/forgot-password',
+			name: 'ForgotPassword',
+			component: ViewForgotPassword
 		}
 	]
 });
 
 router.beforeEach(async (to, from) => {
 	const storeAuth = useStoreAuth();
+
+	storeAuth.authErrorMessage = '';
+
+	if (!storeAuth.user.id && to.name === 'ForgotPassword') {
+		return true;
+	}
 
 	if (!storeAuth.user.id && to.name !== 'Auth') {
 		return {
